962c61badc8312dc96eb484918b7bd6862edf180
Michael Shank <mikeshank@gmail.com>
2019-05-23 21:08:36 -0400

    Updates

--- precache-manifest.c42c9dc7e777d41c1e31d7d573e005b9.js
+++ precache-manifest.c42c9dc7e777d41c1e31d7d573e005b9.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/barnyard-sharks/static/js/runtime~main.ad91d716.js"
   },
   {
-    "revision": "ce97c73f74db9d0fc8ab",
-    "url": "/barnyard-sharks/static/js/main.7d73fc3e.chunk.js"
+    "revision": "ad634bd00971860883fb",
+    "url": "/barnyard-sharks/static/js/main.44bc4c4a.chunk.js"
   },
   {
     "revision": "ab75c669672f2c3e7efb",
     "url": "/barnyard-sharks/static/js/2.6b4884df.chunk.js"
   },
   {
-    "revision": "ce97c73f74db9d0fc8ab",
-    "url": "/barnyard-sharks/static/css/main.aeb4395d.chunk.css"
+    "revision": "ad634bd00971860883fb",
+    "url": "/barnyard-sharks/static/css/main.5a948051.chunk.css"
   },
   {
-    "revision": "1d7cbb399bd38d8a1746f499ca17bbba",
+    "revision": "fab1b69736870165e1477664e35916d7",
     "url": "/barnyard-sharks/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/barnyard-sharks/precache-manifest.cab7c9414fa51efc6c6d807061e477f0.js"
+  "/barnyard-sharks/precache-manifest.c42c9dc7e777d41c1e31d7d573e005b9.js"
 );
 
 workbox.clientsClaim();
