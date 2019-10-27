f60c7f511d59f01a1bacb95cc946e5125dbfbe54
Michael Shank <mikeshank@gmail.com>
2019-10-27 14:16:51 -0400

    adding font for offline

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-
+import 'typeface-metal-mania';
 import './index.css';
 import App from './components/App';
 
