29389f31c321a4ebd71925976d2813637b012f51
Michael Shank <mikeshank@gmail.com>
2019-11-17 18:31:23 -0500

    Updates

---
+++ precache-manifest.48461e3566a09eda98802de6e0b17f05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "99a3c3252ac8219db29d702c5cc0a996",
+    "url": "/barnyard-sharks/static/media/metal-mania-latin-400.99a3c325.woff2"
+  },
+  {
+    "revision": "93876aa5257292b325c201f6ed422516",
+    "url": "/barnyard-sharks/static/media/metal-mania-latin-400.93876aa5.woff"
+  },
+  {
+    "revision": "7714a138f5c1fa67a7fa",
+    "url": "/barnyard-sharks/static/js/runtime~main.ad91d716.js"
+  },
+  {
+    "revision": "30749d7ae556d4354244",
+    "url": "/barnyard-sharks/static/js/main.8599dd48.chunk.js"
+  },
+  {
+    "revision": "c9fbd16aa7cf922665a4",
+    "url": "/barnyard-sharks/static/js/2.07666d0e.chunk.js"
+  },
+  {
+    "revision": "30749d7ae556d4354244",
+    "url": "/barnyard-sharks/static/css/main.a9ca45f6.chunk.css"
+  },
+  {
+    "revision": "c9fbd16aa7cf922665a4",
+    "url": "/barnyard-sharks/static/css/2.9503d39a.chunk.css"
+  },
+  {
+    "revision": "233d8646ffe20d05d54d7828d04a3504",
+    "url": "/barnyard-sharks/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/barnyard-sharks/precache-manifest.822fe30e44e45968e614ddc7b2829896.js"
+  "/barnyard-sharks/precache-manifest.48461e3566a09eda98802de6e0b17f05.js"
 );
 
 workbox.clientsClaim();
